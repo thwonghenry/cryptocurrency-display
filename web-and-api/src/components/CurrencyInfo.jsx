f25a9373d50feedb935ab9c7a68e9f73b77ca016
Hin <fraywong@gmail.com>
2017-10-15 00:00:24 +0800

    Add test case on slice number

--- web-and-api/src/components/CurrencyInfo.jsx
+++ web-and-api/src/components/CurrencyInfo.jsx
@@ -1,14 +1,7 @@
 import React, { PureComponent } from 'react';
 import styles from './CurrencyInfo.scss';
 import cn from 'classnames';
-
-const sliceNumber = (number, length) => {
-    let string = number.toString().slice(0, length);
-    if (string[string.length - 1] === '.') {
-        string = number.toString(0, -1);
-    }
-    return string;
-};
+import sliceNumber from '../lib/sliceNumber';
 
 class CurrencyInfo extends PureComponent {
     render () {
